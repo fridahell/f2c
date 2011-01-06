dc088067b76d91fd14be55a08d074a662fbdb200
Frida Hellden <frida@austinprkonsult.se>
2011-01-06 17:43:32 +0100

    first commit

---
+++ bin/f2c.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+import sys
+
+def main():
+	try:
+		# Make sure the program is called with exactly one argument.
+		# sys.argv is the number of arguments + 1 because
+		# the program's name is in sys.argv[0]
+		if len(sys.argv) != 2:
+			print "invalid arguments (%d), usage: %s [fahrenheit]" % (len(sys.argv), sys.argv[0])
+			return 1
+
+		# Set fahrenheit variable to first command line argument and convert
+		fahrenheit = float(sys.argv[1])
+
+		# Set celsius variable to the calculated value
+		# we get by converting fahrenheit into celsius using the formula
+		# and round the result to two decimals
+		celsius = round( (fahrenheit - 32) * 5/9, 2 )
+
+		# Write celsius to standard output
+		print celsius
+		return 0
+	except ValueError:
+		print "input is not a valid float, usage: %s [fahrenheit]" % sys.argv[0]
+		return 2
+	except:
+		print "unknown error, usage: %s [fahrenheit]" % sys.argv[0]
+		return 3
+
+sys.exit(main())
